a20567f327bf2d49573027a7e0c8d0ea427a5548
r1FH <91796669+r1FH@users.noreply.github.com>
2023-10-14 16:53:17 +0200

    Update Modified Mobile Q Tool.lua

--- Modified Mobile Q Tool.lua
+++ Modified Mobile Q Tool.lua
@@ -1,12 +1,12 @@
 -- modified the entire script so its more readable for those who wants to put it in their script or skid it
 
-getgenv().fiji = "Q"
+getgenv().bolt = "Q"
 
 local tool = Instance.new("Tool")
 tool.RequiresHandle = false
-tool.Name = "made by fiji.png on discord"
+tool.Name = "made by cognise on discord"
 tool.Activated:Connect(function()
-    game:GetService("VirtualInputManager"):SendKeyEvent(true, getgenv().fiji, false, game)
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, getgenv().bolt, false, game)
 end)
 tool.Parent = game.Players.LocalPlayer.Backpack
 
@@ -16,10 +16,10 @@ end)
 
 game.Players.LocalPlayer.Chatted:Connect(function(message)
     local prefix = "/e key "
-    local newFiji = message:sub(#prefix + 1)
+    local newBolt = message:sub(#prefix + 1)
     if message:sub(1, #prefix) == prefix then
-        getgenv().fiji = newFiji
-        warn("Updated key to:", newFiji)
+        getgenv().bolt = newBolt
+        warn("Updated key to:", newBolt)
     end
 end)
 
